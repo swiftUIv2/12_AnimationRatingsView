1564f7735dc591474849ddefc8177cf82e50e625
emm <723125@gmail.com>
2021-01-17 12:10:33 +0000

    Initial Commit

---
+++ AnimationRatingsView_12/AnimationRatingsView_12App.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationRatingsView_12App.swift
+//  AnimationRatingsView_12
+//
+//  Created by emm on 17/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct AnimationRatingsView_12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
